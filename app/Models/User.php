677c920939e1c57efbda8a4c37508457db6a8a29
Md. Abir Hasan <62834401+AbirHasanShuvo@users.noreply.github.com>
2026-02-17 12:59:53 +0600

    has one and belongs to has been completed

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function userPhone(){
+        return $this->hasOne(Userphone::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Models\User;
+use App\Models\Userphone;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $users = User::all();
+
+    // $userPhone = User::find(2)->userPhone;
+   
+
+    // foreach($user as $joinData){
+    //     return $joinData->name.'<br>'.$joinData->userPhone->id;
+    // }
+
+     $userPhones = Userphone::all();
+    //  foreach($userPhones as $users){
+    //     return $users->userphone."<br>".$users->user->name;
+    //  }
+
+    
+
+    return view('welcome', compact('users'));
+
 });
